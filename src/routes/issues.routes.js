0158e30a659900ce0115cac9c65dd612ebab53b0
Sukoluhle Ndlovu <96921288+SukoluhleNdlovu@users.noreply.github.com>
2026-02-17 19:06:05 +0200

    Update issues.routes.js

---
+++ src/routes/issues.routes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+
+const issuesService = require("../services/issues.service");
+const { validateCreateIssue, validateUpdateIssue } = require("../validators/issues.validators");
+
+// Create
+router.post("/", validateCreateIssue, (req, res, next) => {
+  try {
+    const created = issuesService.create(req.body);
+    res.status(201).json(created);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Get all
+router.get("/", (req, res, next) => {
+  try {
+    const all = issuesService.getAll();
+    res.json(all);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Get one
+router.get("/:id", (req, res, next) => {
+  try {
+    const issue = issuesService.getById(req.params.id);
+    res.json(issue);
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
